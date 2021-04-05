e10968920a74e925c2af0bc1da4fd587680bbd69
Mark Peng <markpeng73@gmail.com>
2021-04-05 16:36:35 +0800

    Revised.

--- bestfitting/src/data_process/s2_resize_png_image.py
+++ bestfitting/src/data_process/s2_resize_png_image.py
@@ -9,11 +9,13 @@ from utils.common_util import *
 from PIL import Image
 
 def do_convert(fname_img):
-    img = np.array(Image.open(opj(png_dir, fname_img)), dtype=np.float32)
+    img = np.array(Image.open(opj(source_dir, fname_img)), dtype=np.float32)
     img = cv2.resize(img, (size, size), interpolation=cv2.INTER_LINEAR)
-    cv2.imwrite(opj(img_dir, fname_img), img)
+    cv2.imwrite(opj(dest_dir, fname_img), img)
 
 parser = argparse.ArgumentParser(description='PyTorch Protein Classification')
+parser.add_argument('--source', type=str, default=DATA_DIR, help='source')
+parser.add_argument('--dest', type=str, default=DATA_DIR, help='dest')
 parser.add_argument('--dataset', type=str, default='train', help='dataset')
 parser.add_argument('--size', type=int, default=1536, help='size')
 args = parser.parse_args()
@@ -23,14 +25,13 @@ if __name__ == '__main__':
 
     size = args.size
     dataset = args.dataset
-    png_dir = opj(DATA_DIR, dataset)
-    # tif_dir = opj(TIF_DIR, dataset, 'tifs')
-    img_dir = opj(DATA_DIR, 'inference', dataset, 'images_%d' % size)
+    source_dir = opj(args.source, dataset)
+    dest_dir = opj(args.dest, 'inference', dataset, 'images_%d' % size)
     n_cpu = 3
 
-    os.makedirs(img_dir, exist_ok=True)
-    start_num = max(0, len(os.listdir(img_dir)) - n_cpu * 2)
-    fnames = np.sort(os.listdir(png_dir))[start_num:]
+    os.makedirs(dest_dir, exist_ok=True)
+    start_num = max(0, len(os.listdir(dest_dir)) - n_cpu * 2)
+    fnames = np.sort(os.listdir(source_dir))[start_num:]
     pool = mlc.SuperPool(n_cpu)
     df_list = pool.map(do_convert, fnames, description='resize %s image' % dataset)
 
